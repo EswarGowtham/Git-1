13b1090eef334b23cf3f4e4c47856368be53d774
EswarGowtham <eswargowtham555@gmail.com>
2023-11-20 12:31:25 +0530

    index page

--- demo.js
+++ demo.js
@@ -1,1 +1,10 @@
-console.log("I Love you");
+var headerTitle=document.getElementById('header-title')
+var header=document.getElementById('main-header')
+header.style.border='solid 3px #000'
+
+
+var items=document.getElementsByClassName('list-group-item')
+console.log(items)
+console.log(items[1])
+items[4].style.fontWeight='bold'
+items[4].style.color='green'
