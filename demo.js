08777057f0b0167389c9f02e74e2073319549f99
EswarGowtham <eswargowtham555@gmail.com>
2023-11-20 17:57:36 +0530

    fourth commit

--- demo.js
+++ demo.js
@@ -7,13 +7,23 @@ var items=document.getElementsByClassName('list-group-item')
 console.log(items)
 console.log(items[1])
 items[4].style.fontWeight='bold';
-items[4].style.color='green';
+items[4].style.color='cyan';
 items[2].style.backgroundColor='green';
 for(var i=0;i<items.length;i++){
     items[i].style.fontWeight='bold';
 }
 
 document.querySelector('.list-group-item').style.color='red';
-document.querySelector('.list-group-item:nth-child(2)').style.color='blue';
+document.querySelector('.list-group-item:nth-child(2)').style.color='green';
 
 document.querySelector('.list-group-item:nth-child(4)').style.color='coral';
+
+ele=document.getElementsByTagName("li")
+console.log(ele)
+
+var titles=document.querySelectorAll('.title');
+console.log(titles)
+var odd=document.querySelectorAll('li:nth-child(odd)')
+for(var i=0;i<odd.length;i++){
+    odd[2].style.backgroundColor='green';
+}
