f04fa6fc92fca8c50c5e711cff35f6a1e3f99f59
EswarGowtham <eswargowtham555@gmail.com>
2023-11-20 17:44:32 +0530

    third commit

--- demo.js
+++ demo.js
@@ -6,5 +6,14 @@ header.style.border='solid 3px #000'
 var items=document.getElementsByClassName('list-group-item')
 console.log(items)
 console.log(items[1])
-items[4].style.fontWeight='bold'
-items[4].style.color='green'
+items[4].style.fontWeight='bold';
+items[4].style.color='green';
+items[2].style.backgroundColor='green';
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
+
+document.querySelector('.list-group-item').style.color='red';
+document.querySelector('.list-group-item:nth-child(2)').style.color='blue';
+
+document.querySelector('.list-group-item:nth-child(4)').style.color='coral';
