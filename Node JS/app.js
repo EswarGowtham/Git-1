cee2bca9ab9c78553c15641264c4397e206f4f2e
EswarGowtham <eswargowtham555@gmail.com>
2024-01-05 13:42:12 +0530

    express-parser

--- Node JS/app.js
+++ Node JS/app.js
@@ -1,10 +1,25 @@
 const http = require('http');
-const routes=require('./routes')
-console.log(routes.someText)
-const server = http.createServer(routes.handler);
+const exp=require('express')
+const bodyparser=require('body-parser')
+const app=exp()
+
+app.use(bodyparser.urlencoded({extended:false}));
+
+
+app.use('/add-product',(req,res,next)=>{
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+})
+
+app.use('/product',(req,res,next)=>{
+  console.log(req.body);
+  res.redirect('/');
+})
+app.use('/',(req,res,next)=>{
+  res.send('<h1>Hello from Express</h1>')
+})
+const server = http.createServer(app);
 
 const PORT = 4000;
-
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
