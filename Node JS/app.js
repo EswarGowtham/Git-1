0f96dfa5ff386229f5bbe21dced016856f73b96a
EswarGowtham <eswargowtham555@gmail.com>
2024-01-05 19:03:33 +0530

    route filter

--- Node JS/app.js
+++ Node JS/app.js
@@ -1,25 +1,24 @@
 const http = require('http');
-const exp=require('express')
+const exp =require('express')
 const bodyparser=require('body-parser')
 const app=exp()
+const PORT = 4000;
+const adminRoutes=require('./routes/admin')
+
+const shoproutes=require('./routes/shop')
 
 app.use(bodyparser.urlencoded({extended:false}));
 
+app.use('/admin',adminRoutes)
+app.use('/shop',shoproutes)
 
-app.use('/add-product',(req,res,next)=>{
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+app.use((req,res,next)=>{
+  res.status(404).send('<h1>Page not found</h1>')
 })
 
-app.use('/product',(req,res,next)=>{
-  console.log(req.body);
-  res.redirect('/');
-})
-app.use('/',(req,res,next)=>{
-  res.send('<h1>Hello from Express</h1>')
-})
 const server = http.createServer(app);
 
-const PORT = 4000;
+
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
