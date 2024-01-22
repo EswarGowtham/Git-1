201e7bd17ab588fe71143b6f2559c21e62699a11
EswarGowtham <eswargowtham555@gmail.com>
2024-01-22 15:07:02 +0530

    relationships

--- Node JS/app.js
+++ Node JS/app.js
@@ -1,10 +1,14 @@
 const path = require('path');
-
+const port =4000
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -17,16 +21,48 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findById(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
 sequelize
+
   .sync()
   .then(result => {
+    return User.findById(1);
     // console.log(result);
-    app.listen(3000);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
+    app.listen(port, () => {
+      console.log(`Server listening at http://localhost:${port}`);
+    })
   })
   .catch(err => {
     console.log(err);
